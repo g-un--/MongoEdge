82de39fa7731f2e8094c2dc000422959c4fe6915
Gino Ungureanu <gino.ungureanu@teamnet.ro>
2014-08-01 08:47:09 +0300

    Demo for working with MongoDB from nodejs calling edgejs

---
+++ MongoEdge/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoEdge
+{
+    public static class Test
+    {
+        public static Func<Task> WithStopwatch(this Func<Task> task, string name)
+        {
+            return () =>
+            {
+                var stopwatch = Stopwatch.StartNew();
+                return task().ContinueWith(_ =>
+                    {
+                        stopwatch.Stop();
+                        Console.WriteLine(string.Format("test: {0}. ms: {1}", name, stopwatch.ElapsedMilliseconds));
+                    });
+            };
+        }
+    }
+}
